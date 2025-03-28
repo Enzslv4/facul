a1a0e3074745696f53777270f888e953d49aa055
Enzo Silva <enzosilva142002@gmail.com>
2025-03-27 21:00:24 -0300

    roteiro 1 de java adicionado à programação de sol comp

---
+++ semestre_5/programacao_de_solucoes_computacionais/roteiro1/helloworld.java
@@ -0,0 +1,8 @@
+package semestre_5.programacao_de_solucoes_computacionais.roteiro1;
+
+public class helloworld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ semestre_5/programacao_de_solucoes_computacionais/roteiro1/parte2.java
@@ -0,0 +1,11 @@
+package semestre_5.programacao_de_solucoes_computacionais.roteiro1;
+
+public class parte2 {
+    public static void main(String[] args) {
+        double sb = 2500;
+        int numHorasExtras = 10;
+
+        System.out.println("Salario base: " + sb);
+        System.out.println("Numero de horas extras: " + numHorasExtras);
+    }
+}
