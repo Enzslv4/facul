20eba4fe49ef32a0e90df66aa998f13659c800e2
Enzo Silva <enzosilva142002@gmail.com>
2025-04-26 12:05:57 -0300

    fechando scanner no roteiro 2 parte 3

--- semestre_5/programacao_de_solucoes_computacionais/projectEstudoOO/src/roteiro2/parte3/Programa05.java
+++ semestre_5/programacao_de_solucoes_computacionais/projectEstudoOO/src/roteiro2/parte3/Programa05.java
@@ -6,6 +6,7 @@ public class Programa05 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         int cont = 0;
+        entrada.close();
         for (cont = 0; cont < 5; cont++) {
             System.out.println("Informe uma nota: ");
             int nota = entrada.nextInt();
@@ -16,7 +17,7 @@ public class Programa05 {
                 System.out.println("Reprovado.");
             }
         }
-        entrada.close();
+        
         
     }
 }
