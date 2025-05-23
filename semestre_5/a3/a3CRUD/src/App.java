529147b8903afabd85e9068dc5b8ab765f33e0ab
Enzo Silva <enzosilva142002@gmail.com>
2025-05-22 21:50:59 -0300

    refazendo

--- semestre_5/a3/a3CRUD/src/App.java
+++
@@ -1,7 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        javax.swing.SwingUtilities.invokeLater(() -> {
-            new Interface().setVisible(true);
-        });
-    }
-}
