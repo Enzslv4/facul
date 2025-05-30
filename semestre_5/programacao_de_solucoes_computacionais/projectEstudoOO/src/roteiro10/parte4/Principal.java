399a5073c6f48bf6073c67e9106414eccf49ac4f
Enzo Silva <enzosilva142002@gmail.com>
2025-05-30 15:16:45 -0300

    tudo feito

---
+++ semestre_5/programacao_de_solucoes_computacionais/projectEstudoOO/src/roteiro10/parte4/Principal.java
@@ -0,0 +1,12 @@
+package roteiro10.parte4;
+
+public class Principal {
+    public static void main(String[] args) {
+        JanelaSemLayout janela01 = new JanelaSemLayout();
+    }
+
+}
+/* 5 - Não, apenas o botão 1
+ * 6 - O botão 2 vai funcionar também
+ * 7 - Agora cada botão se identifica e fala a classe
+ */
